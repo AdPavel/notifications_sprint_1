ad619604ff3f1d228bd7a4cd9d656fa161a35cb5
PavelAd <adap2@mail.ru>
2023-04-19 15:35:52 +0400

    fix: new paths

---
+++ srv_admin_panel/app/config/components/common.py
@@ -0,0 +1,17 @@
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', False) == 'True'
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+ROOT_URLCONF = 'config.urls'
+WSGI_APPLICATION = 'config.wsgi.application'
+
+LANGUAGE_CODE = 'ru-RU'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
