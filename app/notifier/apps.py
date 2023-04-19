7ffe910dd4079c3c5879275fc499ab7a14efec85
PavelAd <adap2@mail.ru>
2023-04-19 15:30:51 +0400

    fix: new path

--- app/notifier/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotifierConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notifier'
-    verbose_name = 'Отправка уведомлений'
