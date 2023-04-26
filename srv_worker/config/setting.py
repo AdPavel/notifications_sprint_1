73ebad291c224f71ad2ef0f232bf70d7f67d90ed
PavelAd <adap2@mail.ru>
2023-04-26 20:14:00 +0400

    build: add settings to connect to PG

--- srv_worker/config/setting.py
+++ srv_worker/config/setting.py
@@ -6,4 +6,10 @@ class Settings(BaseSettings):
     rabbit_host: str
     queue_names: list = list['email', 'sms', 'push']
 
+    db_name: str = 'notifications_db'
+    db_user: str = 'admin'
+    db_password: str = 'admin'
+    db_host: str = '127.0.0.1'
+    db_port: int = 5432
+
 settings = Settings()
