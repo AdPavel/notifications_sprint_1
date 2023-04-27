f9b8d9bce079ac2c6fcd93a1ecd16ea320b9af94
PavelAd <adap2@mail.ru>
2023-04-27 23:59:46 +0400

    fix: pg database name

--- srv_admin_panel/config/components/database.py
+++ srv_admin_panel/config/components/database.py
@@ -1,7 +1,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
