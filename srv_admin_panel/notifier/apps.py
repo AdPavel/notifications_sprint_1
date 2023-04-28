ab879d062af5f698e2a31618341826fa1457b280
elena <ep.yudzina@gamil.com>
2023-04-28 14:52:47 +0500

    add registration notification

--- srv_admin_panel/notifier/apps.py
+++ srv_admin_panel/notifier/apps.py
@@ -5,3 +5,6 @@ class NotifierConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifier'
     verbose_name = 'Отправка уведомлений'
+
+    def ready(self):
+        import notifier.signals
