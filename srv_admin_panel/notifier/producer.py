877816c6aafb3fa389d9840029879d926e1179eb
elena <ep.yudzina@gamil.com>
2023-04-28 16:17:42 +0500

    add sending messages to rabbit

---
+++ srv_admin_panel/notifier/producer.py
@@ -0,0 +1,28 @@
+import json
+
+import pika
+
+
+class RabbitPublisher:
+    def __init__(self, connection_params, queue_name, max_priority):
+        self.connection_params = connection_params
+        self.queue_name = queue_name
+        self.max_priority = max_priority
+
+    def publish(self, message, priority):
+
+        parameters = pika.URLParameters(self.connection_params)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        channel.queue_declare(
+            queue=self.queue_name,
+            arguments={"x-max-priority": self.max_priority},
+            durable=True
+        )
+        channel.basic_publish(
+            properties=pika.BasicProperties(priority=priority),
+            exchange='',
+            routing_key=self.queue_name,
+            body=json.dumps(message)
+        )
+        connection.close()
