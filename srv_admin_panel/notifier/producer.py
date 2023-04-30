7dd41dc607f9eb928bae62a1f8218bded19c6198
PavelAd <adap2@mail.ru>
2023-04-30 12:40:52 +0400

    file: producer.py
    fix: add annotation
    
    Closes: #43

--- srv_admin_panel/notifier/producer.py
+++ srv_admin_panel/notifier/producer.py
@@ -4,19 +4,19 @@ import pika
 
 
 class RabbitPublisher:
-    def __init__(self, connection_params, queue_name, max_priority):
+    def __init__(self, connection_params: str, queue_name: str, max_priority: int):
         self.connection_params = connection_params
         self.queue_name = queue_name
         self.max_priority = max_priority
 
-    def publish(self, message, priority):
+    def publish(self, message: dict, priority: int):
 
         parameters = pika.URLParameters(self.connection_params)
         connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
         channel.queue_declare(
             queue=self.queue_name,
-            arguments={"x-max-priority": self.max_priority},
+            arguments={'x-max-priority': self.max_priority},
             durable=True
         )
         channel.basic_publish(
