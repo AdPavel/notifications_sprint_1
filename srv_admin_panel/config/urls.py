392bf6f4a996cf5c097a1c456214b4aed91288f8
PavelAd <adap2@mail.ru>
2023-04-30 12:41:31 +0400

    file: admin.py, urls.py
    fix: replace " to '

--- srv_admin_panel/config/urls.py
+++ srv_admin_panel/config/urls.py
@@ -6,6 +6,6 @@ from .api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", api.urls)
+    path('api/', api.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
